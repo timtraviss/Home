f077c86e5b9a337ea126daee6c0869a61055debc
timtraviss <57478589+timtraviss@users.noreply.github.com>
2021-03-25 12:56:43 +1300

    Update test.php

--- test.php
+++ test.php
@@ -2,16 +2,27 @@
 In the form include the <form Action="name.php" method="post"></form>
 
 --> 
+<form action="name.php" method="post>
+<textarea cols="20" rows ="50" name="comment">
+<?php
+echo $current;
+?>
+</textarea>
+           <input type="submit">                     
+</form>
 
-
-?php
-
+<?php
+// This is the code that checks the of the file exists and then creates the file from the textarea. 
 if (file_exists("log.txt")) { 
 $file = "log.txt";
 $current = file_gets_contents($file); 
-} else {
+} 
+else {
 $myfile = fopen ("log.txt", "w");
 header("refresh:0");
 
 }
 ?>
+
+                               
+                               
