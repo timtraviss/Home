282ca90586c623b1fb919a647c0ff83d8f4e13e5
timtraviss <57478589+timtraviss@users.noreply.github.com>
2021-03-25 14:07:47 +1300

    Update test.php

--- test.php
+++ test.php
@@ -29,5 +29,9 @@ header("refresh:0");
 $comment = $_POST["comment"];
 //Get the file 
 $file = "log.txt";
+//write the new text file 
+file_put_contents($file, $comments);
+//send the user back to index pages to view changes      
+header('location: http://localhost/');
 ?>
                                
