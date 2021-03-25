9670de0922cf2a68f14e66f9ab34316c20077832
timtraviss <57478589+timtraviss@users.noreply.github.com>
2021-03-25 14:17:32 +1300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<! -- Php script for saving files as txt files. 
+In the form include the <form Action="name.php" method="post"></form>
+--> 
+
+
+<?php
+// This is the code that checks the of the file exists and then creates the file from the textarea. 
+if (file_exists("log.txt")) { 
+$file = "log.txt";
+$current = file_gets_contents($file); 
+} 
+else {
+$myfile = fopen ("log.txt", "w");
+header("refresh:0");
+}
+
+<form action="name.php" method="post>
+<textarea cols="20" rows ="50" name="comment">
+<?php
+echo $current;
+?>
+</textarea>
+           <input type="submit">                     
+</form>
+?>
